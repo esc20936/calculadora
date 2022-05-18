b1c2ebcd065a8df1ea308942ab43bd0d161e394c
esc20936 <esc20936@uvg.edu.gt>
2022-05-18 13:28:24 -0600

    funcional

--- src/Componentes/Button.jsx
+++ src/Componentes/Button.jsx
@@ -1,11 +1,13 @@
+/* eslint-disable react/forbid-prop-types */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from "react";
 import "./Button.css";
+import PropTypes from "prop-types";
 
 export default function Button({
-  texto, style, textoStyle, valor, fValor,fOp
+  texto, style, textoStyle, valor, fValor, fOp, sRe,
 }) {
   const butttonClick = (e) => {
     let valorB = "";
@@ -14,17 +16,27 @@ export default function Button({
     } else if (e.target.className === "textoBoton") {
       valorB = e.target.innerText;
     }
-    if (["+", "-", "*", "/", "="].includes(valorB)) {
+    if (["+", "-", "*", "/"].includes(valorB)) {
       fOp(valorB);
     } else {
       const data = valor;
-      fValor(data + valorB);
+      fValor((sRe) ? valorB : data + valorB);
     }
   };
 
   return (
     <div className="CuerpoBoton" style={style} onClick={((e) => butttonClick(e))}>
-      <h1 className="textoBoton" style={textoStyle} onClick={() => { null; }}>{ texto }</h1>
+      <h1 className="textoBoton" style={textoStyle} onClick={() => null}>{ texto }</h1>
     </div>
   );
 }
+
+Button.propTypes = {
+  style: PropTypes.object.isRequired,
+  textoStyle: PropTypes.object.isRequired,
+  valor: PropTypes.string.isRequired,
+  fValor: PropTypes.func.isRequired,
+  fOp: PropTypes.func.isRequired,
+  sRe: PropTypes.bool.isRequired,
+  texto: PropTypes.string.isRequired,
+};
--- src/Componentes/Calculadora.jsx
+++ src/Componentes/Calculadora.jsx
@@ -1,52 +1,79 @@
+/* eslint-disable no-eval */
+/* eslint-disable no-param-reassign */
 /* eslint-disable react/jsx-no-bind */
 import React, { useState } from "react";
 import "./Calculadora.css";
 import Pantalla from "./Pantalla";
 import Button from "./Button";
 
+let stack = ["", null, null];
+let sRes = false;
+
 export default function Calculadora() {
   const [valor, setValor] = useState("");
 
   function actualizarValor(newValue) {
-    setValor(newValue);
+    if (sRes) {
+      setValor("");
+      setValor(newValue);
+      sRes = false;
+    } else {
+      setValor(newValue);
+    }
   }
 
   function cleanValor(newValue) {
-    setValor("");
+    stack = ["", null, null];
+    sRes = false;
+    newValue = "";
+    setValor(newValue);
   }
 
   function operacion(Operando) {
-    /* TODO: Implement operacion con string */
+    const val = stack[0];
+    if (val === "") {
+      stack = [parseInt(valor, 10), Operando, null];
+      setValor("");
+    } else {
+      sRes = true;
+      const res = eval(stack[0] + stack[1] + parseInt(valor, 10));
+      if (res > 999999999) {
+        alert("Error: Se reinicio la calculadora");
+        cleanValor("");
+      } else {
+        setValor(res);
+        stack = [res, Operando, null];
+      }
+    }
   }
 
   return (
     <div className="cuerpo">
       <Pantalla valor={valor} />
       <div className="row1">
-        <Button valor={valor} fOp={operacion} fValor={actualizarValor} texto="+" textoStyle={{ color: "orange" }} />
-        <Button valor={valor} fOp={operacion} fValor={actualizarValor} texto="-" textoStyle={{ color: "orange" }} />
-        <Button valor={valor} fOp={operacion} fValor={actualizarValor} texto="/" textoStyle={{ color: "orange" }} />
-        <Button valor={valor} fOp={operacion} fValor={actualizarValor} texto="*" textoStyle={{ color: "orange" }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="+" textoStyle={{ color: "orange" }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="-" textoStyle={{ color: "orange" }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="/" textoStyle={{ color: "orange" }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="*" textoStyle={{ color: "orange" }} style={{ }} />
       </div>
       <div className="row1">
-        <Button valor={valor} fValor={actualizarValor} texto="1" />
-        <Button valor={valor} fValor={actualizarValor} texto="2" />
-        <Button valor={valor} fValor={actualizarValor} texto="3" />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="1" textoStyle={{ }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="2" textoStyle={{ }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="3" textoStyle={{ }} style={{ }} />
       </div>
       <div className="row1">
-        <Button valor={valor} fValor={actualizarValor} texto="4" />
-        <Button valor={valor} fValor={actualizarValor} texto="5" />
-        <Button valor={valor} fValor={actualizarValor} texto="6" />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="4" textoStyle={{ }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="5" textoStyle={{ }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="6" textoStyle={{ }} style={{ }} />
       </div>
       <div className="row1">
-        <Button valor={valor} fValor={actualizarValor} texto="7" />
-        <Button valor={valor} fValor={actualizarValor} texto="8" />
-        <Button valor={valor} fValor={actualizarValor} texto="9" />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="7" textoStyle={{ }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="8" textoStyle={{ }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="9" textoStyle={{ }} style={{ }} />
       </div>
       <div className="row1">
-        <Button valor={valor} fValor={actualizarValor} texto="0" />
-        <Button valor={valor} fOp={operacion} fValor={actualizarValor} texto="=" textoStyle={{ color: "orange" }} />
-        <Button valor={valor} fOp={operacion} fValor={cleanValor} texto="C" textoStyle={{ color: "orange" }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={actualizarValor} texto="0" textoStyle={{ }} style={{ }} />
+        <Button valor={valor} fOp={operacion} sRe={sRes} fValor={cleanValor} style={{ width: "140px" }} texto="C" textoStyle={{ color: "orange" }} />
       </div>
     </div>
   );
